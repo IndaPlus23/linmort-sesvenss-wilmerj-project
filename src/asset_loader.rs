4bba272d2f7b47958969c06e50737a07dfcc3fab
SEBBSITER <58857890+SEBBSITER@users.noreply.github.com>
2024-04-19 14:29:27 +0200

    Add temporary solution to load textures by name

--- src/asset_loader.rs
+++ src/asset_loader.rs
@@ -1,9 +1,13 @@
+use bevy::asset::LoadedAsset;
 use bevy::prelude::*;
+
+const SAMPLE_ASSETS: Vec<String> = Vec::new();
 
 /// SceneAssets stores handles for assets used in the scene.
 #[derive(Resource, Debug, Default)]
 pub struct SceneAssets {
     pub enemy: Handle<Scene>,
+    pub textures: Vec<Handle<Image>>,
 }
 
 pub struct AssetLoaderPlugin;
@@ -19,10 +23,23 @@ impl Plugin for AssetLoaderPlugin {
 fn load_assets(mut scene_assets: ResMut<SceneAssets>, asset_server: Res<AssetServer>) {
     *scene_assets = SceneAssets {
         enemy: asset_server.load("models/Rock.glb#Scene0"),
+        textures: load_textures_from_folder(SAMPLE_ASSETS, asset_server),
     }
 }
 
-/// Loads a spritesheet given number of columns and rows
+/// Loads folder of textures and upgrades into handle of image
+fn load_textures_from_folder(assets: Vec<String>, asset_server: Res<AssetServer>) -> Vec<Handle<Image>> {
+    let mut image_handles: Vec<Handle<Image>> = Vec::new();
+
+    for asset in assets.iter() {
+        // Unchecked loading
+        let asset: Handle<Image> = asset_server.load(format!("{}{}", "textures/", asset));
+        image_handles.push(asset);
+    }
+
+    return image_handles
+}
+
 fn load_sprite_sheet(
     asset_server: Res<AssetServer>,
     mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
@@ -33,7 +50,7 @@ fn load_sprite_sheet(
     offset: Option<Vec2>,
     file_name: String,
 ) -> Handle<TextureAtlasLayout> {
-    let texture = asset_server.load("spritesheet.png");
+    let texture: Handle<Image> = asset_server.load("spritesheet.png");
     let layout = TextureAtlasLayout::from_grid(tile_size, columns, rows, None, None);
     texture_atlas_layouts.add(layout)
 }
