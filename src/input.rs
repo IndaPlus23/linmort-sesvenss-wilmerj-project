466bea9c2fd1d13ab780324347e16cd7bac9bab3
BuildTools <unconfigured@null.spigotmc.org>
2024-04-22 15:33:58 +0200

    modified:   wall collision now works with updated dev

--- src/input.rs
+++ src/input.rs
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-use bevy::{
-    input::mouse::MouseMotion,
-    sprite::Mesh2dHandle, // ADDED FOR BASIC WALL COLLISION
-    prelude::*,
-};
-use std::f32::consts::PI;
-use crate::Player;
-use crate::structures::Wall; // ADDED FOR BASIC WALL COLLISION
-
-=======
-use crate::Player;
+use crate::{floor, Player};
+use crate::wall::Wall; // wall_collision
+use crate::floor::Floor; // wall_collision
+use crate::CustomMaterial; // wall_collision
+use bevy::{prelude::*, render::mesh::Mesh, sprite::Mesh2dHandle}; // wall_collision
 use bevy::window::{CursorGrabMode, PrimaryWindow};
 use bevy::{input::mouse::MouseMotion, prelude::*};
 use std::f32::consts::PI;
->>>>>>> dev
 
 #[derive(Default)]
 pub struct MouseState {
@@ -28,8 +20,22 @@ pub fn keyboard_input(
     mut query: Query<&mut Player>,
     time: Res<'_, Time<Real>>,
 
-    // ADDED wall_query TO CHECK IF POSITION OF PLAYER IS THE SAME AS WALL FOR BASIC WALL COLLISION
-    mut wall_query: Query<(&mut Wall, &mut Transform, &mut Mesh2dHandle)>, 
+    // ADDED wall_query TO CHECK IF POSITION OF PLAYER IS THE SAME AS WALL/FLOOR FOR BASIC WALL COLLISION
+    mut wall_query: Query<(
+        &mut Wall,
+        &mut Transform,
+        &mut Mesh2dHandle,
+        &mut Handle<CustomMaterial>,
+    )>,
+    mut floor_query: Query<
+        (
+            &mut Floor,
+            &mut Transform,
+            &mut Mesh2dHandle,
+            &mut Handle<CustomMaterial>,
+        ),
+        Without<Wall>,
+    >,
 ) {
     if keyboard_input.just_pressed(KeyCode::Escape) {
         std::process::exit(0);
@@ -49,6 +55,10 @@ pub fn keyboard_input(
             // also hide the cursor
             primary_window.cursor.visible = false;
         }
+    }
+
+    if keyboard_input.just_pressed(KeyCode::KeyN) {
+
     }
 
     for mut player in query.iter_mut() {
@@ -83,9 +93,14 @@ pub fn keyboard_input(
         
 
         // CHECKS EVERY WALL FOR COLLISION
-         for (wall, _transform, _mesh2dhandle) in wall_query.iter_mut() {
+        for (wall, _transform, _mesh2dhandle, _material_handle) in wall_query.iter_mut() {
             wall_collision(&wall, &mut movement, &player);
-         }
+        }
+        // CHECKS EVERY FLOOR FOR COLLISION
+        for (floor, _transform, _mesh2dhandle, _material_handle) in floor_query.iter_mut() {
+            floor_collision(&floor, &mut movement, &player);
+        }
+
 
         player.x += movement.x;
         player.y += movement.y;
@@ -113,22 +128,94 @@ pub fn keyboard_input(
 */
 
 pub fn wall_collision(wall: &Mut<'_, Wall>, movement: &mut bevy::prelude::Vec3, player: &bevy::prelude::Mut<'_, Player>) {
+    let padding = 1.5;
+
+    let mut x_hit = false;
+    let mut y_hit = false;
+    let mut z_hit = false;
+    
+    // CHECK IF PLAYER HITS WALL IN x DIRECTION
+    let position_x = player.x + movement.x;
+    let wall_start_x = wall.start.position.x - padding;
+    let wall_end_x = wall.end.position.x + padding;
+
+    // YOU CAN ADD A WALL WHERE THE BEGINING IS SMALLER OR BIGGER THAN THE END, THIS IF/ELSE MAKES SURE WALL COLLISION STILL WORKS THEN
+    if wall_start_x < wall_end_x {
+        if position_x > wall_start_x && position_x < wall_end_x {
+            x_hit = true
+        }
+    } else {
+        if position_x < wall_start_x && position_x > wall_end_x {
+            x_hit = true
+        }
+    }
+
+    // CHECK IF PLAYER HITS WALL IN y DIRECTION
+    let position_y = player.y + movement.y;
+    let wall_start_y = wall.start.position.y - padding;
+    let wall_end_y = wall.end.position.y + wall.height + padding;
+    
+    // YOU CAN ADD A WALL WHERE THE BEGINING IS SMALLER OR BIGGER THAN THE END, THIS IF/ELSE MAKES SURE WALL COLLISION STILL WORKS THEN
+    if wall_start_y < wall_end_y {
+        if position_y > wall_start_y && position_y < wall_end_y {
+            y_hit = true
+        }
+    } else {
+        if position_y < wall_start_y && position_y > wall_end_y {
+            y_hit = true
+        }
+    }
+    
+    // CHECK IF PLAYER HITS WALL IN z DIRECTION
+    let position_z = player.z + movement.z;
+    let wall_start_z = wall.start.position.z - padding;
+    let wall_end_z = wall.end.position.z + padding;
+
+    // YOU CAN ADD A WALL WHERE THE BEGINING IS SMALLER OR BIGGER THAN THE END, THIS IF/ELSE MAKES SURE WALL COLLISION STILL WORKS THEN
+    if wall_start_z < wall_end_z {
+        if position_z > wall_start_z && position_z < wall_end_z {
+            z_hit = true;
+        }
+    } else {
+        if position_z < wall_start_z && position_z > wall_end_z {
+            z_hit = true;
+        }
+    }
+
+    if x_hit && y_hit && z_hit {
+        
+        // logic so that a player can "slide" against the wall
+        if wall.start.position.x - wall.end.position.x == 0. {
+            movement[0] = 0.
+        }
+        if wall.start.position.y - wall.end.position.y == 0. {
+            movement[1] = 0.
+        }
+        if wall.start.position.z - wall.end.position.z == 0. {
+            movement[2] = 0.
+        }
+    }
+}
+
+pub fn floor_collision(floor: &Mut<'_, Floor>, movement: &mut bevy::prelude::Vec3, player: &bevy::prelude::Mut<'_, Player>) {
     let padding = 1.0;
 
     let mut x_hit = false;
     let mut y_hit = false;
     let mut z_hit = false;
+/* 
+    floor.a.position.x
 
     // positiv X åt höger
-    if player.x + movement.x > wall.start.x - padding && player.x + movement.x < wall.end.x + padding {
+    if player.x + movement.x > wall.start.position.x - padding && player.x + movement.x < wall.end.position.x + padding {
         x_hit = true
     }
     //positiv Y uppåt
-    if player.y + movement.y > wall.start.y - padding && player.y + movement.y < wall.end.y + wall.height + padding {
+    if player.y + movement.y > wall.start.position.y - padding && player.y + movement.y < wall.end.position.y + wall.height + padding {
         y_hit = true
     }
     // -Z i framåtriktningen
-    if player.z + movement.z > wall.start.z - padding && player.z + movement.z < wall.end.z + padding {
+    if player.z + movement.z > wall.start.position.z - padding && player.z + movement.z < wall.end.position.z + padding {
         z_hit = true;
     }
 
@@ -139,16 +226,16 @@ pub fn wall_collision(wall: &Mut<'_, Wall>, movement: &mut bevy::prelude::Vec3, 
     if x_hit && y_hit && z_hit {
         
         // logic so that a player can "slide" against the wall
-        if wall.start.x - wall.end.x == 0. {
+        if wall.start.position.x - wall.end.position.x == 0. {
             movement[0] = 0.
         }
-        if wall.start.y - wall.end.y == 0. {
+        if wall.start.position.y - wall.end.position.y == 0. {
             movement[1] = 0.
         }
-        if wall.start.z - wall.end.z == 0. {
+        if wall.start.position.z - wall.end.position.z == 0. {
             movement[2] = 0.
         }
-    }
+    } */
 }
 
 pub fn mouse_input(
