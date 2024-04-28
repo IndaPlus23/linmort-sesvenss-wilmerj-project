91860d7d376eebef1b66509edace2b6879c15832
BuildTools <unconfigured@null.spigotmc.org>
2024-04-28 10:44:47 +0200

    added height and gravity to player.rs

--- src/player.rs
+++ src/player.rs
@@ -1,22 +1,26 @@
 use bevy::prelude::*;
 
-#[derive(Component, Clone)]
+#[derive(Default, Component, Clone, PartialEq)]
 pub struct Player {
     pub x: f32,
     pub y: f32,
     pub z: f32,
     pub yaw: f32,
     pub pitch: f32,
+    pub height: f32,
+    pub gravity: f32,
 }
 
 impl Player {
-    pub fn new(x: f32, y: f32, z: f32, yaw: f32, pitch: f32) -> Self {
+    pub fn new(x: f32, y: f32, z: f32, yaw: f32, pitch: f32, height: f32, gravity: f32) -> Self {
         Self {
             x,
             y,
             z,
             yaw,
             pitch,
+            height,
+            gravity
         }
     }
 }
